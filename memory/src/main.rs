ce3b077aee790ddb58ed202836f9af69c502c6b2
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-02-25 21:03:14 +0900

    feat: 16進数を確認

--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let hex1: u32 = 0xDEADBEEF;
     println!("n1: {:p}", &hex1);
-
-    print!("DEBUG STOP!");
+    println!("n1 size: {:?} bytes", std::mem::size_of_val(&hex1));
+    println!("STOP");
 }
