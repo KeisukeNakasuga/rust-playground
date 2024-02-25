2ee91770e7f534b92d494405f24714ea2f976911
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-02-25 20:33:49 +0900

    feat: 16進数を確認

--- memory/README.md
+++ memory/README.md
@@ -1,2 +1,8 @@
 # memory
 Rustのオブジェクトがメモリ上でどのように展開されているのかを観察する。
+
+# 16進数
+```rust
+let hex1: u32 = 0xDEADBEEF;
+```
+![](resource/hex.png)
--- memory/src/main.rs
+++ memory/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let hex1: u32 = 0xDEADBEEF;
+    println!("n1: {:p}", &hex1);
+
+    print!("DEBUG STOP!");
 }
